71ab4d7ce996e7454dabb799e10c0d72e18867ba
aese7 <ahmedd.eissa7@gmail.com>
2025-01-05 17:53:27 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, this is your deployed Flask app on Heroku!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
